dc2fa836e2f754ec26615bee2a6fac882c7a5b2d
Nik Swap <davsebamse@gmail.com>
2021-01-15 22:02:29 +0100

    Added more sql examples. Remember to remove limit to see all results

--- all_with_com_in_forward.sql
+++ all_with_com_in_forward.sql
@@ -9,6 +9,10 @@ FROM
     domainnames.id = forwards.domainid
 WHERE
     INSTR(forward_to,REPLACE(domain,'www.','')) = 0
+	AND
+    INSTR(forward_to,'.com') > 0
+	AND
+    INSTR(forward_to, substr(REPLACE(domain,'www.',''),1,INSTR(REPLACE(domain,'www.',''),'.'))) = 0
 ORDER BY
     domainnames.added_at DESC
 LIMIT 30
