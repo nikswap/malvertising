de9c922fcff444a303f541ed4ef2f5e4cd28dcd0
Nik Swap <davsebamse@gmail.com>
2021-01-16 17:01:17 +0100

    Added some sql that should give hits good

---
+++ all_without_dk_in_forward.sql
@@ -0,0 +1,21 @@
+SELECT
+        domainnames.added_at
+    ,   forwards.added_at
+    ,    domain
+    ,   forward_to
+FROM
+    domainnames
+    INNER JOIN forwards ON
+    domainnames.id = forwards.domainid
+WHERE
+    INSTR(forward_to,REPLACE(domain,'www.','')) = 0
+	AND
+    INSTR(forward_to,'.dk') = 0
+	AND
+    INSTR(forward_to, substr(REPLACE(domain,'www.',''),1,INSTR(REPLACE(domain,'www.',''),'.'))) = 0
+	AND
+    INSTR(forward_to, 'http') > 0
+ORDER BY
+    domainnames.added_at DESC
+LIMIT 30
+;
